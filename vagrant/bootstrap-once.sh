a602496cb1b9a02db38e9446b16d90b5bb6d53cb
Tom Causs <tom.causs@ordina.be>
2015-05-25 20:58:33 +0200

    add Gradle wrapper

--- vagrant/bootstrap-once.sh
+++ vagrant/bootstrap-once.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# update apt
-apt-get update
+echo "updating packages..."
+apt-get update -qq
 
-# setup couchdb
-apt-get install couchdb -y
+echo "setting up CouchDB..."
+apt-get install -y -qq couchdb > /dev/null
 
 # configure couchdb
-sed -i 's/^;bind_address.*/bind_address = 0.0.0.0/' /etc/couchdb/local.ini
+sed -i 's/^;bind_address = .*/bind_address = 0.0.0.0/' /etc/couchdb/local.ini
 
 # restart couchdb to apply changes
 service couchdb restart
+
+echo "access CouchDB via: http://localhost:5984/_utils/"
