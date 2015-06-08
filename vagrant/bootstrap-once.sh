bd942b2c3e0b5dfafe55fdd5a6e6e794fb533eee
Tom Causs <tom.causs@ordina.be>
2015-06-08 17:54:08 +0200

    import sample data on startup

--- vagrant/bootstrap-once.sh
+++ vagrant/bootstrap-once.sh
@@ -12,4 +12,8 @@ sed -i 's/^;bind_address = .*/bind_address = 0.0.0.0/' /etc/couchdb/local.ini
 # restart couchdb to apply changes
 service couchdb restart
 
+echo "importing sample data..."
+sleep 5s && curl -XPUT localhost:5984/beer
+curl -XPOST localhost:5984/beer/_bulk_docs -H "Content-Type: application/json" --data-binary @/vagrant/sample_data/beer.json > /dev/null
+
 echo "access CouchDB via: http://localhost:5984/_utils/"
